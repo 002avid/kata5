d91bfe5ef746e135cb388481452bbf9c3151586d
002avid <adbugyf@gmail.com>
2024-12-12 10:51:28 +0000

    Clases de Pojo y User completadas

---
+++ src/main/java/es/ulpgc/is2/control/pojo/RandomUserMeResponse.java
@@ -0,0 +1,8 @@
+package es.ulpgc.is2.control.pojo;
+
+import java.util.List;
+
+public class RandomUserMeResponse {
+    private List<RandomUser> results;
+    public List<RandomUser> getResults() {return results;}
+}
